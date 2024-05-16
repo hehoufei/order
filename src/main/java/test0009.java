a3dbe705f0125fbdb47eefd6af4f343c1dc9f0f0
hehoufei <he.houfei@redstarclouds.com>
2024-05-16 10:14:34 +0800

    第二次

---
+++ src/main/java/test0009.java
@@ -0,0 +1,31 @@
+public class test0009 {
+    public static void main(String[] args) {
+        int[] height = {4,2,0,3,2,5};
+        System.out.printf("" + trap(height));
+    }
+
+    public static int trap(int[] height) {
+        if (height == null || height.length == 2) {
+            System.out.printf("hhh ");
+            return 0;
+        }
+        int result = 0;
+        int left = 1;
+        int right = height.length - 2;
+        int leftMax = height[0];
+        int rightMax = height[height.length - 1];
+        while (left <= right) {
+            if (leftMax <= rightMax) {
+                result += Math.max(leftMax - height[left], 0);
+                leftMax = Math.max(leftMax, height[left]);
+                left ++;
+            } else {
+                result += Math.max(rightMax - height[right], 0);
+                rightMax = Math.max(rightMax, height[right]);
+                right --;
+            }
+
+        }
+        return result;
+    }
+}
